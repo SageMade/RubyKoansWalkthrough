2cd09401712640f0f9cb43b20097a95c3fbd0578
ShawnM427 <me@shawnm.ca>
2017-03-02 17:43:19 -0500

    Completed about_triangle_project_2.rb
    - Added exceptions to the triangle function
    - Now throws exceptions on invalid triangles

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,16 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  if a <= 0 || b <= 0 || c <= 0
+  	raise TriangleError, "Cannot have a length less than or equal to zero"
+  elsif !check_side(a, b, c)
+  	raise TriangleError, "Side a violates the Triangle inequality rule"
+  elsif !check_side(b, a, c)
+  	raise TriangleError, "Side b violates the Triangle inequality rule"
+  elsif !check_side(c, a, b)
+  	raise TriangleError, "Side c violates the Triangle inequality rule"
+  end
+
   if a == b && b == c
   	return :equilateral
   elsif a != b and b != c and a != c
@@ -21,6 +31,10 @@ def triangle(a, b, c)
   else 
   	return :isosceles
   end
+end
+
+def check_side(side, other, other_other)
+	return side < other + other_other && side >= (other - other_other).abs
 end
 
 # Error class used in part 2.  No need to change this code.
